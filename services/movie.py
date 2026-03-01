3726765222b82161a79d468a2674626f9437f284
Oleh Bozhak <olbozhak@gmail.com>
2026-03-01 15:54:06 +0200

    Implemented methods for Movie, CinemaHall, MovieSession

---
+++ services/movie.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from db.models import Movie
+from django.db.models import QuerySet
+
+
+def get_movies(
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None
+) -> QuerySet[Movie]:
+    qs = Movie.objects.all()
+
+    if genres_ids is not None and actors_ids is not None:
+        qs = qs.filter(
+            genres__id__in=genres_ids,
+            actors__id__in=actors_ids
+        ).distinct()
+    elif genres_ids is not None:
+        qs = qs.filter(genres__id__in=genres_ids).distinct()
+    elif actors_ids is not None:
+        qs = qs.filter(actors__id__in=actors_ids).distinct()
+    return qs
+
+
+def get_movie_by_id(movie_id: int) -> Movie:
+    return Movie.objects.filter(id=movie_id).first()
+
+
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids: int = None,
+        actors_ids: int = None
+) -> None:
+    movie = Movie.objects.create(
+        title=movie_title, description=movie_description
+    )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+
+    if actors_ids:
+        movie.actors.set(actors_ids)
---
+++ services/movie_session.py
@@ -0,0 +1,57 @@
+from db.models import MovieSession, Movie, CinemaHall
+from datetime import date
+from django.db.models import QuerySet
+
+
+def create_movie_session(
+        movie_show_time: date,
+        movie_id: int,
+        cinema_hall_id: int
+) -> None:
+    movie = Movie.objects.get(id=movie_id)
+    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+
+    MovieSession.objects.create(
+        show_time=movie_show_time,
+        cinema_hall=cinema_hall,
+        movie=movie
+    )
+
+
+def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
+    ms = MovieSession.objects.all()
+
+    if session_date:
+        convert = tuple([int(i) for i in session_date.split("-")])
+        ms = ms.filter(show_time__date=date(*convert))
+
+    return ms
+
+
+def get_movie_session_by_id(movie_id: int) -> MovieSession | None:
+    return MovieSession.objects.filter(id=movie_id).first()
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: date = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+) -> None:
+    ms = get_movie_session_by_id(session_id)
+
+    if show_time:
+        ms.show_time = show_time
+
+    if movie_id:
+        ms.movie_id = movie_id
+
+    if cinema_hall_id:
+        ms.cinema_hall_id = cinema_hall_id
+
+    ms.save()
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    ms = get_movie_session_by_id(session_id)
+    ms.delete()
